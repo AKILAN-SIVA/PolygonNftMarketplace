83e8e9cb236fd5118660f92fe36f7a66f0bf12fa
Akilan <akilanlakshmi2003@gmail.com>
2023-07-25 19:12:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className='w-screen h-screen flex justify-center items-center'>
+        <h1 className='text-4xl font-bold'>Nft Marketplace</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
