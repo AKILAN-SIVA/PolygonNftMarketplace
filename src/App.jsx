3771af394635f82abd74df0a80a91e83ee3ac1d9
Bhoopesh <bhoopesh.ad21@bitsathy.ac.in>
2023-07-25 23:45:12 +0530

    create and navbar page updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./pages/Navbar"
+import Create from "./Create";
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <div>
         <Navbar />
+        <Create />
       </div>
     </>
   )
