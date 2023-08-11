86e01a6a2807512ad1c806fc4a615398881055ec
Kuralamudhan74 <kuralamu04@gmail.com>
2023-08-11 09:14:42 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoo(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
