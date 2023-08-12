6c3796029e8f774bd6fce612ccbe4beb5182a00a
Akilan <109203235+AKILAN-SIVA@users.noreply.github.com>
2023-08-12 12:17:53 +0530

    Search dialog box created

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,73 +1,59 @@
 import React from "react";
 import { useState } from "react";
 import Navbar from "../pages/Navbar"
-import { MdCloudUpload } from "react-icons/md"
 
 export const Create = () => {
   const [image, setImage] = useState(null)
   const [filename, setFilename] = useState()
   return (
-    <div className="w-full h-full flex items-center justify-center bg-black">
-      {/* <div className='pt-12'>
+    <div className="bg-black text-white">
+      <div className='pt-12'>
         <Navbar />
-      </div> */}
-      <div className="flex flex-col w-full h-full justify-center items-center ">
-        <div className="max-w-[600px] w-full">
+      </div>
+      <div className="flex w-full h-full justify-center items-center ml-24 mt-12">
+        <div className="w-full">
           <div>
-            <p className="font-bold text-3xl flex  items-center justify-center ">
+            <p className="font-bold text-3xl flex  items-center justify-start ">
               Create New NFT
             </p>
-            <div className="flex gap-2 p-4">
+            {/* <div className="flex gap-2">
               <p className="text-red-700">*</p>
               <p>Required</p>
-            </div>
-            <div className="flex flex-col gap-4 p-4">
-              <div className="flex gap-1">
+            </div> */}
+            <div className="flex flex-col gap-2 mt-8">
+              <div className="flex gap-1 text-lg">
                 <p>Name</p>
                 <p className="text-red-700">*</p>
               </div>
               <input
-                className="flex flex-col p-2 rounded-xl bg-transparent border-gray-400 border-2"
+                className="flex flex-col rounded-xl bg-white border-gray-400 border-2 h-12 w-[700px] p-4"
                 type="text"
                 placeholder="Certificate name"
               ></input>
             </div>
-            <div className="flex flex-col gap-4 p-4">
+            <div className="flex flex-col gap-2 mt-4">
               <div className="flex gap-1">
                 <p>Roll Number</p>
                 <p className="text-red-700">*</p>
               </div>
               <input
-                className="flex flex-col p-2 rounded-xl bg-transparent border-gray-400 border-2"
+                className="flex flex-col rounded-xl  bg-white border-gray-400 border-2 h-12 w-[700px] p-4"
                 type="text"
                 placeholder="Student roll no"
               ></input>
             </div>
-            <div className="flex flex-col gap-4 p-4">
+            <div className="flex flex-col gap-2 mt-4">
               <div className="flex gap-1">
                 <p>College / University</p>
                 <p className="text-red-700">*</p>
               </div>
               <input
-                className="flex flex-col p-2 rounded-xl bg-transparent border-gray-400 border-2"
+                className="flex flex-col  rounded-xl  bg-white border-gray-400 border-2 h-12 w-[700px] p-4"
                 type="text"
                 placeholder="Institute name"
               ></input>
             </div>
-            <div className="flex flex-col gap-4 p-4">
-              <div className="flex gap-1">
-                <p>Description</p>
-                <p className="text-red-700">*</p>
-              </div>
-              <textarea
-                className="flex flex-col p-2 bg-transparent border-gray-400 border-2 rounded-xl"
-                placeholder="Student Details" rows={6}
-              ></textarea>
-            </div>
-            {/* <div className="w-[350px] h-[250px] border-2 border-dashed border-gray-400 flex justify-center items-center p-4 m-4 ml-28 cursor-pointer" onClick={() => document.querySelector(".input-field").click()}>
-              <input className="input-field hidden" type="file" value={image} onChange={''}></input>
-              {image ? <img src={image} alt={filename} /> : <MdCloudUpload size={30} />}
-            </div> */}
+
           </div>
         </div>
       </div>
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,22 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Search from "../assets/search.png";
 
 function Navbar() {
+
   const [walletAddress, setWalletAddress] = useState("");
-  let [connected, setConnected] = useState(false);
+  const [connected, setConnected] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [tokenId, setTokenId] = useState("");
+
   const ConnectWallet = async () => {
     if (window.ethereum) {
-      window.ethereum.request({ method: "eth_requestAccounts" }).then((res) => {
-        console.log(res);
-        setConnected(true);
-        setWalletAddress(res);
-      });
+      window.ethereum.request({ method: "eth_requestAccounts" })
+        .then((accounts) => {
+          console.log(accounts[0]);
+          setWalletAddress(accounts[0]);
+          setConnected(true);
+        });
     } else {
       alert("Install Metamask Extension");
     }
   };
+
+  const getNFT = async () => {
+    console.log(tokenId);
+    setShowModal(false);
+  }
 
   return (
     <>
@@ -40,11 +50,53 @@ function Navbar() {
               </Link>
             </div>
             <div className="flex justify-end py-2 gap-6">
-              <button className="">
+              <button className="" onClick={() => setShowModal(true)}>
                 <img src={Search} alt="SearchIcon" className="h-7 w-7" />
               </button>
+              {showModal ? (
+                <>
+                  <div
+                    className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                  >
+                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                      {/*content*/}
+                      <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-300 outline-none focus:outline-none">
+                        {/*header*/}
+                        <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                          <h3 className="text-3xl font-semibold">
+                            Search any NFTs
+                          </h3>
+                          <button
+                            className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                            onClick={() => setShowModal(false)}
+                          >
+                            <span className="bg-transparent text-black h-6 w-6 text-3xl block outline-none focus:outline-none">
+                              ×
+                            </span>
+                          </button>
+                        </div>
+                        {/*body*/}
+                        <div className="relative p-6 flex-auto">
+                          <input type="number" onChange={(e) => setTokenId(e.target.value)} className="bg-transparent w-[600px] h-12 text-black rounded-lg border-2 border-black p-4" placeholder="Enter Token Id" value={tokenId}/>
+                        </div>
+                        {/*footer*/}
+                        <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                          <button
+                            className="bg-black text-white active:bg-black font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                            type="button"
+                            onClick={() => getNFT()}
+                          >
+                            Search
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                </>
+              ) : null}
               <button className="bg-black text-white border rounded-lg w-[170px] h-12 hover:bg-gray-700 text-lg" onClick={ConnectWallet}>
-                {connected ? "Connected" : "Connect Wallet"}
+                {walletAddress != "" ? "Connected" : "Connect Wallet"}
               </button>
             </div>
           </div>
