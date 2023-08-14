66e7601319fc7ddfe5f22adfb9f344b8de5a623c
Bhoopesh <105773196+bhoopesh-sankar@users.noreply.github.com>
2023-08-14 15:20:00 +0530

    updated create page and nft uploaded to pinata

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -2,17 +2,94 @@ import React from "react";
 import { useState } from "react";
 import Navbar from "../pages/Navbar";
 import PreviewImage from "../assets/previewImage.png";
+import { uploadFileToIPFS,uploadJSONToIPFS } from "./Pinata";
+import { ethers } from "ethers";
+import Marketplace from "../Marketplace.json"
 
 export const Create = () => {
   const [form, setForm] = useState({
     title: "",
     description: "",
     collection: "",
+    price: "",
     photo: "",
   });
 
+  const [fileURL,setFileURL] = useState(null)
+
   const handleChange = (e) =>
     setForm({ ...form, [e.target.name]: e.target.value });
+  
+    async function OnChangeFile(e) {
+      var file = e.target.files[0];
+      //check for file extension
+      try {
+          //upload the file to IPFS
+          const response = await uploadFileToIPFS(file);
+          if(response.success === true) {
+              console.log("Uploaded image to Pinata: ", response.pinataURL)
+              setFileURL(response.pinataURL);
+          }
+      }
+      catch(e) {
+          console.log("Error during file upload", e);
+      }
+  }
+
+  //This function uploads the metadata to IPFS
+  async function uploadMetadataToIPFS() {
+      const {title,collection, price, description} = form;
+      //Make sure that none of the fields are empty
+      if( !title|| !collection || !description || !price || !fileURL)
+          return;
+
+      const nftJSON = {
+          title, collection,price,description,  image: fileURL
+      }
+
+      try {
+          //upload the metadata JSON to IPFS
+          const response = await uploadJSONToIPFS(nftJSON);
+          if(response.success === true){
+              console.log("Uploaded JSON to Pinata: ", response)
+              return response.pinataURL;
+          }
+      }
+      catch(e) {
+          console.log("error uploading JSON metadata:", e)
+      }
+  }
+
+  async function listNFT(e) {
+      e.preventDefault();
+
+      //Upload data to IPFS
+      try {
+          const metadataURL = await uploadMetadataToIPFS();
+          //After adding your Hardhat network to your metamask, this code will get providers and signers
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          const signer = provider.getSigner();
+
+          //Pull the deployed contract instance
+          let contract = new ethers.Contract(Marketplace.address, Marketplace.abi, signer)
+
+          //massage the params to be sent to the create NFT request
+          const price = form.price;
+
+          //actually create the NFT
+          let transaction = await contract.mint(metadataURL, price)
+          await transaction.wait()
+
+          alert("Successfully listed your NFT!");
+          setForm({ title: '',collection: '', description: '', price: '',photo: ''});
+          window.location.replace("/")
+      }
+      catch(e) {
+          console.log( "Upload error"+e )
+      }
+  }
+
+  // console.log("Working", process.env);
 
   return (
     <div className="bg-black text-white min-h-screen justify-center">
@@ -57,6 +134,20 @@ export const Create = () => {
 
             <div className="flex flex-col gap-2 ">
               <div className="flex gap-2 text-lg">
+                <p>Price</p><p className="text-red-800">*</p>
+              </div>
+              <input
+                className="flex flex-col rounded-xl bg-transparent border-gray-400 border-2 h-12 w-[650px] p-4"
+                type="number"
+                name="price"
+                value={form.price}
+                placeholder="Enter price for Listing. . ."
+                onChange={handleChange}
+              ></input>
+            </div>
+
+            <div className="flex flex-col gap-2 ">
+              <div className="flex gap-2 text-lg">
                 <p>Description</p><p className="text-red-800" >*</p>
               </div>
               <input
@@ -86,12 +177,12 @@ export const Create = () => {
               <input
                 type="file"
                 name="photo"
-                onChange={handleChange}
+                onChange={OnChangeFile}
                 value={form.photo}
               />
             </div>
             <div className="mt-8 pb-8">
-              <button className="bg-white text-black h-12 w-full font-bold text-xl rounded-lg">
+              <button className="bg-white text-black h-12 w-full font-bold text-xl rounded-lg" onClick={listNFT}>
                 Create
               </button>
             </div>
---
+++ src/pages/Pinata.jsx
@@ -0,0 +1,94 @@
+//require('dotenv').config();
+import axios from 'axios';
+import {FromData} from 'form-data'
+const key = '44e5ce4a21c24521e44a';
+const secret = '2b14f5bc6c2a314d6f9d3b06ea37c78d46a81e9bcce6f0cfc88a45bfddcd80fc';
+
+
+
+
+export const uploadJSONToIPFS = async(JSONBody) => {
+    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    return axios 
+        .post(url, JSONBody, {
+            headers: {
+                pinata_api_key: key,
+                pinata_secret_api_key: secret,
+            }
+        })
+        .then(function (response) {
+           return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
+
+export const uploadFileToIPFS = async(file) => {
+    const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    
+    let data = new FormData();
+    data.append('file', file);
+
+    const metadata = JSON.stringify({
+        name: 'testname',
+        keyvalues: {
+            exampleKey: 'exampleValue'
+        }
+    });
+    data.append('pinataMetadata', metadata);
+
+    //pinataOptions are optional
+    const pinataOptions = JSON.stringify({
+        cidVersion: 0,
+        customPinPolicy: {
+            regions: [
+                {
+                    id: 'FRA1',
+                    desiredReplicationCount: 1
+                },
+                {
+                    id: 'NYC1',
+                    desiredReplicationCount: 2
+                }
+            ]
+        }
+    });
+    data.append('pinataOptions', pinataOptions);
+
+    return axios 
+        .post(url, data, {
+            maxBodyLength: 'Infinity',
+            headers: {
+                'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                pinata_api_key: key,
+                pinata_secret_api_key: secret,
+            }
+        })
+        .then(function (response) {
+            console.log("image uploaded", response.data.IpfsHash)
+            alert("image uploaded")
+            return {
+               success: true,
+               pinataURL: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+           };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+
+    });
+};
