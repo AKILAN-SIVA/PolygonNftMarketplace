ebeead207503ac7208395b2dabded2b345fec4ad
Bhoopesh <105773196+bhoopesh-sankar@users.noreply.github.com>
2023-09-22 17:27:31 +0530

    Bidding updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -222,8 +222,44 @@ export const Home = () => {
         </>
       ) : (
         <>
+          {/* <div className="container">
+              <div className="loadbar" style={{ width: `${loadingProgress}%` }}></div>
+              <div id="slide" ref={slideRef}>
+                {allNfts.map((value, index) => (
+                  value.format == "1" ?
+                    <div
+                      key={index}
+                      className="item"
+                      style={{ backgroundImage: `url(${value.photo})`, borderRadius: '30px', }}
+                      onClick={() => navigate("/searchNft", { state: value })}
+                    >
+                      <div className="content">
+                        <div className="bg-[#3c3d3c] grid gap-6 justify-center items-center rounded-xl p-4">
+                          <div className="text-7xl font-bold text-gray-100 tracking-wider">{value.title}</div>
+                          <div className="flex gap-1 text-3xl font-semibold text-gray-100"><img src={AddressIcon} className='h-8 w-8' /> {value.price}</div>
+                        </div>
 
-          {/* <div className="max-w-100% h-[700px] w-full m-auto py-16 px-24 relative group">
+                      </div>
+                    </div>
+                    :
+                    <></>
+                ))}
+              </div>
+
+              <div className="buttons">
+                <div className="flex justify-center gap-6">
+                  <button id="prev" onClick={handleClickPrev}>
+                    <FontAwesomeIcon icon={faAngleLeft} />
+                  </button>
+                  <button id="next" onClick={handleClickNext}>
+                    <FontAwesomeIcon icon={faAngleRight} />
+                  </button>
+                </div>
+              </div>
+            </div> */}
+          {/* <div className="w-full min-h-screen"></div> */}
+
+          <div className="max-w-100% h-[700px] w-full m-auto py-16 px-24 relative group">
             <div
               style={{ backgroundImage: `url(${onlyImgNfts[currentIndex].photo})` }}
               className="w-full h-full rounded-2xl bg-center bg-cover duration-500 hover:cursor-pointer"
@@ -242,7 +278,7 @@ export const Home = () => {
             <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-white text-black cursor-pointer">
               <BsChevronCompactRight onClick={nextSlide} size={30} />
             </div>
-          </div> */}
+          </div>
 
           <div className="flex justify-center ">
             <RiSeparator size={80} />
--- src/pages/ViewnftBidding.jsx
+++ src/pages/ViewnftBidding.jsx
@@ -9,7 +9,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import Identicon from "react-identicons";
-import Music from "../assets/music.png"
+import Music from "../assets/music.png";
 import { FaCirclePlay } from "react-icons/fa6";
 import { FaCirclePause } from "react-icons/fa6";
 
@@ -29,8 +29,10 @@ export const ViewnftBidding = () => {
   const [highestBidderAddress, updateHighestBidderAddress] = useState("0x");
   const [timesec, setTime] = useState();
   const [play, setPlay] = useState(false);
+  const [reportMsg, setReportMsg] = useState("");
+  const [showReportModal, setShowReportModal] = useState(false);
 
-  let audio = new Audio(state.data.photo)
+  let audio = new Audio(state.data.photo);
   const myRef = useRef(audio);
   const startAudio = () => {
     myRef.current.play();
@@ -72,7 +74,8 @@ export const ViewnftBidding = () => {
       );
       const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      const totsec = (days * 24 * 60 * 60) + (hours * 60 * 60) + (minutes * 60) + seconds;
+      const totsec =
+        days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds;
       setTime(totsec);
       // console.log(totsec);
       if (distance < 0) {
@@ -85,7 +88,6 @@ export const ViewnftBidding = () => {
       }
     }, 1000);
   };
-
 
   useEffect(() => {
     startTimer();
@@ -108,7 +110,7 @@ export const ViewnftBidding = () => {
     );
 
     let highestBidder = await contract.getHighestBidder(state.data.biddingId);
-    console.log(highestBidder, "is highest bidder")
+    console.log(highestBidder, "is highest bidder");
     updateHighestBidderAddress(highestBidder);
 
     //create an NFT Token
@@ -145,7 +147,7 @@ export const ViewnftBidding = () => {
 
   if (!dataFetched) getNFTData();
   console.log(data);
-  console.log("highestBidderAddress", highestBidderAddress)
+  console.log("highestBidderAddress", highestBidderAddress);
   console.log(address);
   // console.log(timesec);
 
@@ -232,6 +234,30 @@ export const ViewnftBidding = () => {
 
   console.log(state.data.status);
   let circleCommonClasses = "h-6 w-6 bg-current  rounded-full";
+
+  const report = async () => {
+    if (reportMsg == "") {
+      alert("Enter reason");
+      return;
+    }
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
+    //Pull the deployed contract instance
+    let contract = new ethers.Contract(
+      Marketplace.address,
+      Marketplace.abi,
+      signer
+    );
+    let transaction = await contract.UserReportNFT(
+      state.data.tokenId,
+      reportMsg
+    );
+    await transaction.wait();
+    alert("reported");
+    window.location.replace("/");
+  };
+
   return (
     <div className="bg-black min-h-screen h-fit w-full text-white">
       <div className="pt-12">
@@ -253,71 +279,145 @@ export const ViewnftBidding = () => {
         <>
           <div className="grid justify-start mt-12 ml-24 gap-8">
             <div className="flex justify-start gap-16">
-              {
-                state.data.format == "1" ?
-                  <>
-
-                    <div className='border-2 border-gray-600 w-[700px] h-[700px] rounded-3xl shadow-md overflow-hidden'>
-                      <img src={state.data.photo} className='w-full h-full' />
-                    </div>
-                  </>
-                  :
-                  <></>
-              }
-              {
-                state.data.format == "2" ?
-                  <>
-                    <div className='relative border-2 border-gray-600 w-[700px] h-[700px] rounded-3xl shadow-md '>
-                      <img src={Music} className='w-full h-full rounded-3xl' />
-                      {
-                        play ?
-                          <button className="absolute bottom-4 right-4 rounded-full  flex justify-center items-center p-2" onClick={pauseAudio}><FaCirclePause size={40} /></button>
-                          :
-                          <button className="absolute bottom-4 right-4 rounded-full  flex justify-center items-center p-2" onClick={startAudio}><FaCirclePlay size={40} /></button>
-                      }
-                    </div>
-                  </>
-                  :
-                  <></>
-              }
-              {
-                state.data.format == "3" ?
-                  <>
-                    <div className='border-2 border-gray-600 w-[700px] h-[700px] rounded-3xl shadow-md overflow-hidden'>
-                      <video src={state.data.photo} className='w-full h-full' controls loop autoPlay onLoadedMetadata={handleLoadedMetadata} controlsList='nodownload' />
-                    </div>
-                  </>
-                  :
-                  <></>
-              }
+              {state.data.format == "1" ? (
+                <>
+                  <div className="border-2 border-gray-600 w-[700px] h-[700px] rounded-3xl shadow-md overflow-hidden">
+                    <img src={state.data.photo} className="w-full h-full" />
+                  </div>
+                </>
+              ) : (
+                <></>
+              )}
+              {state.data.format == "2" ? (
+                <>
+                  <div className="relative border-2 border-gray-600 w-[700px] h-[700px] rounded-3xl shadow-md ">
+                    <img src={Music} className="w-full h-full rounded-3xl" />
+                    {play ? (
+                      <button
+                        className="absolute bottom-4 right-4 rounded-full  flex justify-center items-center p-2"
+                        onClick={pauseAudio}
+                      >
+                        <FaCirclePause size={40} />
+                      </button>
+                    ) : (
+                      <button
+                        className="absolute bottom-4 right-4 rounded-full  flex justify-center items-center p-2"
+                        onClick={startAudio}
+                      >
+                        <FaCirclePlay size={40} />
+                      </button>
+                    )}
+                  </div>
+                </>
+              ) : (
+                <></>
+              )}
+              {state.data.format == "3" ? (
+                <>
+                  <div className="border-2 border-gray-600 w-[700px] h-[700px] rounded-3xl shadow-md overflow-hidden">
+                    <video
+                      src={state.data.photo}
+                      className="w-full h-full"
+                      controls
+                      loop
+                      autoPlay
+                      onLoadedMetadata={handleLoadedMetadata}
+                      controlsList="nodownload"
+                    />
+                  </div>
+                </>
+              ) : (
+                <></>
+              )}
               <div className="grid justify-start items-start h-fit gap-8 mt-8">
                 <div className="flex justify-between items-center">
-                  <div className='flex justify-start items-center gap-4'>
-                    <span className='text-2xl font-bold'>Owned by</span>
-                    <div className='bg-purple-500  w-fit flex justify-between h-12 items-center rounded-2xl px-2'>
-                      <div className='flex items-center gap-1'>
-                        <img src={AddressIcon} className='h-6 w-6' />
-                        <h1 className='text-white justify-start text-lg'>{(state.data.owner).substring(0, 6)}....{(state.data.owner).substring(36, 42)}</h1>
+                  <div className="flex justify-start items-center gap-4">
+                    <span className="text-2xl font-bold">Owned by</span>
+                    <div className="bg-purple-500  w-fit flex justify-between h-12 items-center rounded-2xl px-2">
+                      <div className="flex items-center gap-1">
+                        <img src={AddressIcon} className="h-6 w-6" />
+                        <h1 className="text-white justify-start text-lg">
+                          {state.data.owner.substring(0, 6)}....
+                          {state.data.owner.substring(36, 42)}
+                        </h1>
                       </div>
                     </div>
                   </div>
                   <div>
-                    {
-                      timerDays == "00" &&
-                        timerHours == "00" &&
-                        timerMinutes == "00" &&
-                        timerSeconds == "00" ?
-                        <>
-                          <h1 className='flex justify-center items-center bg-red-600 p-4 text-white text-lg h-10 rounded-xl'>Expired</h1>
-                        </>
-                        :
-                        <>
-                          <h1 className='flex justify-center items-center bg-green-600 p-4 text-white text-lg h-10 rounded-xl'>Active</h1>
-                        </>
-                    }
+                    {timerDays == "00" &&
+                    timerHours == "00" &&
+                    timerMinutes == "00" &&
+                    timerSeconds == "00" ? (
+                      <>
+                        <h1 className="flex justify-center items-center bg-red-600 p-4 text-white text-lg h-10 rounded-xl">
+                          Expired
+                        </h1>
+                      </>
+                    ) : (
+                      <>
+                        <div className="flex gap-4">
+                          <h1 className="flex justify-center items-center bg-green-600 p-4 text-white text-lg h-12 rounded-xl">
+                            Active
+                          </h1>
+                          <div>
+                            <button
+                              className="flex justify-center items-center bg-white h-12 w-32 text-black text-xl rounded-2xl"
+                              onClick={() => setShowReportModal(true)}
+                            >
+                              Report
+                            </button>
+                          </div>
+                        </div>
+                      </>
+                    )}
                   </div>
                 </div>
-
+                {showReportModal ? (
+                  <>
+                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                      <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                        {/*content*/}
+                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-300 outline-none focus:outline-none">
+                          {/*header*/}
+                          <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                            <h3 className="text-3xl font-semibold text-black">
+                              Report this NFT
+                            </h3>
+                            <button
+                              className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                              onClick={() => setShowReportModal(false)}
+                            >
+                              <span className="bg-transparent text-black h-6 w-6 text-3xl block outline-none focus:outline-none">
+                                ×
+                              </span>
+                            </button>
+                          </div>
+                          {/*body*/}
+                          <div className="relative p-6 flex-auto">
+                            <input
+                              type="text"
+                              onChange={(e) => setReportMsg(e.target.value)}
+                              className="bg-transparent w-[600px] h-12 text-black rounded-lg border-2 border-black p-4"
+                              placeholder="Enter valid reason"
+                              value={reportMsg}
+                            />
+                          </div>
+                          {/*footer*/}
+                          <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                            <button
+                              className="bg-black text-white active:bg-black font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                              type="button"
+                              onClick={() => report()}
+                            >
+                              Report
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                  </>
+                ) : null}
                 {state.data.Buyer == null ? (
                   <div></div>
                 ) : (
@@ -336,9 +436,9 @@ export const ViewnftBidding = () => {
                   Price: {state.data.price}
                 </span>
                 {timerDays == "00" &&
-                  timerHours == "00" &&
-                  timerMinutes == "00" &&
-                  timerSeconds == "00" ? (
+                timerHours == "00" &&
+                timerMinutes == "00" &&
+                timerSeconds == "00" ? (
                   <></>
                 ) : (
                   <span className="text-xl ">
@@ -366,12 +466,13 @@ export const ViewnftBidding = () => {
                 <div className="flex flex-col gap-2 ">
                   <div className="flex gap-2 text-lg"></div>
                   {timerDays == "00" &&
-                    timerHours == "00" &&
-                    timerMinutes == "00" &&
-                    timerSeconds == "00" ? (
+                  timerHours == "00" &&
+                  timerMinutes == "00" &&
+                  timerSeconds == "00" ? (
                     <>
                       {(address == highestBidderAddress ||
-                        address == state.data.owner) && state.data.status != 0 ? (
+                        address == state.data.owner) &&
+                      state.data.status != 0 ? (
                         <button
                           className="text-black bg-white rounded-xl w-full h-12 font-bold cursor-pointer mt-4 tracking-widest"
                           onClick={CompleteBidding}
@@ -384,8 +485,9 @@ export const ViewnftBidding = () => {
                             <>
                               {data.map((value, index) => {
                                 if (
-                                  (address == value.bidder &&
-                                    value.biddingId == state.data.biddingId) && (address != highestBidderAddress)
+                                  address == value.bidder &&
+                                  value.biddingId == state.data.biddingId &&
+                                  address != highestBidderAddress
                                 ) {
                                   return (
                                     <div>
@@ -408,32 +510,30 @@ export const ViewnftBidding = () => {
                     </>
                   ) : (
                     <>
-                      {
-                        address == state.data.owner ?
-                          <></>
-                          :
-                          <div>
-                            <p className="pb-4 text-xl">
-                              Bid Price <span className="text-red-800">*</span>
-                            </p>
+                      {address == state.data.owner ? (
+                        <></>
+                      ) : (
+                        <div>
+                          <p className="pb-4 text-xl">
+                            Bid Price <span className="text-red-800">*</span>
+                          </p>
 
-                            <input
-                              className="flex flex-col rounded-xl bg-transparent border-gray-400 border-2 h-12 w-full p-4"
-                              type="number"
-                              name="collection"
-                              onChange={(e) => setBidPrice(e.target.value)}
-                              placeholder="Make Bid . . ."
-                              value={BidPrice}
-                            ></input>
-                            <button
-                              className="text-black bg-white rounded-xl w-full h-12 font-bold cursor-pointer mt-4 tracking-widest"
-                              onClick={PlaceBid}
-                            >
-                              Place Bid
-                            </button>
-                          </div>
-                      }
-
+                          <input
+                            className="flex flex-col rounded-xl bg-transparent border-gray-400 border-2 h-12 w-full p-4"
+                            type="number"
+                            name="collection"
+                            onChange={(e) => setBidPrice(e.target.value)}
+                            placeholder="Make Bid . . ."
+                            value={BidPrice}
+                          ></input>
+                          <button
+                            className="text-black bg-white rounded-xl w-full h-12 font-bold cursor-pointer mt-4 tracking-widest"
+                            onClick={PlaceBid}
+                          >
+                            Place Bid
+                          </button>
+                        </div>
+                      )}
                     </>
                   )}
                   <div className="pt-12">
@@ -455,13 +555,13 @@ export const ViewnftBidding = () => {
                                     className="bg-gray-900 rounded-xl p-2"
                                   >
                                     <h1 className="text-white justify-start tracking-widest">
-                                      {(value.bidder).substring(0, 7)}....
-                                      {(value.bidder).substring(21, 29)}
+                                      {value.bidder.substring(0, 7)}....
+                                      {value.bidder.substring(21, 29)}
                                     </h1>
                                   </button>
                                 </div>
-                                <div className='flex gap-1 items-center text-xl'>
-                                  <img src={AddressIcon} className='h-6 w-6' />
+                                <div className="flex gap-1 items-center text-xl">
+                                  <img src={AddressIcon} className="h-6 w-6" />
                                   <h1>{value.price}</h1>
                                 </div>
                               </div>
